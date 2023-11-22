69cfd6038bba6710ac3b83050307e197b3edf580
m4ryrl <mari2000rodrigues@gmail.com>
2023-11-22 12:53:24 -0300

    classificador de nível de herói

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 let nome = "HulkaDoGalo";
-let nivel = "ferro";
+let nivel = "platina";
 
 
 switch(nivel){
@@ -12,3 +12,31 @@ switch(nivel){
   case "bronze":
   console.log("O Heroi de nome:", nome, "está no nível de:", nivel)
 }
+
+switch(nivel){
+    case "prata":
+    console.log("O Heroi de nome:", nome, "está no nível de:", nivel)
+  }
+
+  switch(nivel){
+    case "ouro":
+    console.log("O Heroi de nome:", nome, "está no nível de:", nivel)
+  }
+
+  switch(nivel){
+    case "platina":
+    console.log("O Heroi de nome:", nome, "está no nível de:", nivel)
+  }
+
+  switch(nivel){
+    case "ascendente":
+    console.log("O Heroi de nome:", nome, "está no nível de:", nivel)
+  }
+  switch(nivel){
+    case "imortal":
+    console.log("O Heroi de nome:", nome, "está no nível de:", nivel)
+  }
+  switch(nivel){
+    case "radiante":
+    console.log("O Heroi de nome:", nome, "está no nível de:", nivel)
+  }
