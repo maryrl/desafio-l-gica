4bdc3bd9575ab2877677fb0b22ac48a9370c6938
m4ryrl <mari2000rodrigues@gmail.com>
2023-11-22 12:49:48 -0300

    ultimo commit

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("olá mundo");
+let nome = "HulkaDoGalo";
+let nivel = "ferro";
+
+
+switch(nivel){
+  case "ferro":
+  console.log("O Heroi de nome:", nome, "está no nível de:", nivel)
+  break;
+}
+
+switch(nivel){
+  case "bronze":
+  console.log("O Heroi de nome:", nome, "está no nível de:", nivel)
+}
